93ad2b9b8607d70d2776ad9360d9b85d795611f6
Anonim47 <yosef@yosefadi.tk>
2015-10-31 15:35:22 +0700

    vendor: read device list from hudson repository
    Change-Id: I63427f6549ee31e1276461763a547d1e8ced7afa

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,4 @@
-add_lunch_combo xos_armani-userdebug
-
+for combo in $(curl -s https://raw.githubusercontent.com/XoplaxOS/hudson/master/xoplax-build-targets | sed -e 's/#.*$//' | grep xos-2.1 | awk '{printf "xos_%s-%s\n", $1, $2}')
+do
+    add_lunch_combo $combo
+done
